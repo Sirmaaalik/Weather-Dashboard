dde6acc152e3289534a84e525f307d621bf12c70
Sirmaaalik <malikrharris@gmail.com>
2021-12-07 23:16:11 -0500

    Finished the style sheet for the most part and added the calls to the api's to render the temp, humidity, and show symbol (clouds or sun) related to the data shown.

--- index.js
+++ index.js
@@ -70,3 +70,51 @@ function searchClicker() {
         renderButtons();
     })
 }
+
+// 2 API calls 1 for current conditions and 1 for 5 day forecast
+function APIcalls() {
+    url= "https://api.openweathermap.org/data/2.5/forecast?q=";
+    currenturl = "https://api.openweathermap.org/data/2.5/weather?q=";
+
+APIkey="&appid=8b668a5ffbcbc6cece2c6f9eb9ed67bf";
+queryurl = url + city + APIkey;
+currentWeatherUrl= currenturl + city + APIkey;
+
+$("#city-name").text(`${city}`);
+
+// 5 Day Display
+$.ajax({
+    url: queryurl,
+    method: "GET",
+}).then (function(response) {
+    let dayNumber = 0;
+    for(let i=0; i < response.list.length; i++) {
+        if( response.list[i].dt_txt.split(" ")[1]== "12:00:00") {
+            $("#" + dayNumber + "five-day-temp").text("Temperature: "+ Math.round((response.list[i].main.temp-273.15)*9/5 +32)+"F");
+            $("#" + dayNumber + "five-day-humidity").text("Humidity: "+ response.list[i].main.humidity + "%");
+            $("#" + dayNumber + "five-day-icon").attr("src", "http://openweathermap.org/img/w/"+response.list[i].weather[0].icon+".png");
+            console.log(response.list[i].dt_txt.split("-"));
+            console.log(dayNumber);
+            console.log(response.list[i].main.temp);
+            dayNumber++;
+        }
+    }
+});
+
+//Display Data in Main Div
+$.ajax( {
+    url: currentWeatherUrl,
+    method: "GET",
+
+}).then(function(current_data) {
+        console.log(current_data);
+        let temp = Math.round(((current_data.main.temp-273.15)*9/5 +32))
+        console.log("The Temperature in " +city+ " is: " + temp);
+        $("#today-temp").text("Temperature: " + temp + String.fromCharCode(176)+"F");
+        $("#today-humidity").text("Humidity: "+ current_data.main.humidity);
+        $("#today-wind").text(" Wind Speed:"+ current_data.wind.speed);
+        $("today-uv-index").text("UV index:"+ current_data.uvI);
+        $("#today-icon-div").attr({"src": "http://openweathermap.org/img/w/"+ current_data.weather[0].icon+ ".png",
+    "height":"100px", "width": "100px"});
+    })
+}
